10b9ae71214937b95662021079d91d3ae8376fa6
Vindaar <basti90@gmail.com>
2014-06-16 07:01:39 +1000

    commit before report hand in

--- SDSSclasses.py
+++ SDSSclasses.py
@@ -10,62 +10,70 @@ from astropy import units as u
 # Class for the spectra
 class spectrum:
     def __init__(self):
-        self.beginwl     = 0
-        self.deltawl     = 0
-        self.lambmin     = 0
+        self.beginwl     = 0            # wavelength of first pixel in spectrum
+        self.deltawl     = 0            # increment of wavelength per pixel 
+        # lower and upper bound of interval used for compspec scaled to QSOs 
+        # emission redshift: (1+z)*[1095;1150]
+        self.lambmin     = 0            
         self.lambmax     = 0
-        self.cpix        = 0
-        self.z           = 0
+        self.cpix        = 0            # 1
+        self.z           = 0            # emission redshift of the QSO
         # Fitting parameters
-        self.alpha       = 0
-        self.alpha_error = 0
-        self.beta        = 0
-        self.delta       = 0
+        self.alpha       = 0            # spectral index of spectrum
+        self.alpha_error = 0            # 1 sigma error of alpha
+        self.beta        = 0            # alpha = -beta - 2
+        self.delta       = 0            # start of powerlaw wavelength array. 
+                                        # bad name, but taken from C code
         # Coordinates
         # using astropy.coordinates object. Currently using v0.3.2 of astropy
         # from v0.4 (in develeopment atm) on exchanged with SkyCoord. 
         self.coordinates = ICRS(ra = 0, dec = 0, unit=(u.degree, u.degree))
 
-        self.npix           = 0
-        self.flux           = []
-        self.flux_error     = []
-        self.powerlaw       = [] 
-        self.powerlaw_error = []
-        self.wave           = []
-        self.emfree         = []         # Emission free regions array
-        self.mag            = []
-        self.mag_error      = []
-        self.smag           = []
-        self.status         = []
-        self.snr            = []
+        self.npix           = 0         # number of pixels in spectrum
+        # Arrays:
+        self.flux           = []        # flux of spectrum
+        self.flux_error     = []        # 1 sigma of flux
+        self.powerlaw       = []        # powerlaw continuum
+        self.powerlaw_error = []        # 1 sigma of p.c.
+        self.wave           = []        # wavelength 
+        self.emfree         = []        # Emission free regions used for fitting
+        self.mag            = []        # magnitudes in ugriz band
+        self.mag_error      = []        # 1 sigma of magnitudes
+        self.smag           = []        # spectroscopic magnitudes
+        self.status         = []        # status; 1 = good pixel, 0 = bad pixel
+        self.snr            = []        # signal to noise ratio
 
-        self.filename = []
-        self.plateid = 0
-        self.fiberid = 0
-        self.MJD = 0
+        self.filename = []              # Filename of FITS file QSO is stored in
+        self.plateid = 0                # PlateID of spectrum
+        self.fiberid = 0                # FiberID of spectrum
+        self.MJD = 0                    # MJD of spectrum taken on
 
 # Class for the composite spectrum
 # TODO: Think about switching to numpy arrays
 class comp_spectrum:
     def __init__(self, npix):
-        self.wave          = []
-        self.flux          = []
-        self.flux_error    = []
-        self.sum           = []
-        self.sum2          = []
-        self.nhist         = []
-        self.spectra_count = 0
-        self.mean_a        = 0
-        self.sigma_a       = 0
-        self.median_a      = 0
-        self.siqr_a        = 0
+        self.wave          = []         # wavelength
+        self.flux          = []         # actually opacity not flux
+        self.flux_error    = []         # 1 sigma of opacity
+        self.sum           = []         # flux_i / powerlaw_i sum over all i QSOs 
+        self.sum2          = []         # sum of squares of the above
+        self.nhist         = []         # number of contributing spectra to each
+                                        # element in sum and sum2
+        self.spectra_count = 0          # number of used specrtas in total
+        self.mean_a        = 0          # mean of alpha of used spectra
+        self.sigma_a       = 0          # 1 sigma of alpha of used spectra
+        self.median_a      = 0          # median of alpha of used spectra
+        self.siqr_a        = 0          # semi-interquartile range of alpha
         for i in xrange(npix):
-            self.sum.append(0)
+            # initialize sum, sum2 and nhist to 0 and wavelength array starting
+            # from 10**3.58020 Angstrom
+            self.sum.append(0)          
             self.sum2.append(0)
             self.nhist.append(0)
             self.wave.append(10.0**(3.58020 + 0.0001*i))
 
 # Class for the program settings
+
 class program_settings:
     def __init__(self):
         self.dust         = 0
